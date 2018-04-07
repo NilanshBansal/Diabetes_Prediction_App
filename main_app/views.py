dd3aa17c3c7658a90ec173fe7fbefdcb0b8c0442
Nilansh Bansal <bansalnilansh@gmail.com>
2018-04-07 23:11:43 +0530

    predict form added

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 # Create your views here.
-
+import pickle
 
 def index(request):
-    
     return render(request,'index.html',{'title':'Diabetes Detection'})
+
+def test():
+    print ('hello')
+    with open('./bagging_clf.pkl', 'rb') as f:
+        data = pickle.load(f)
+
+    
+
+def predict(request):
+    print (request.POST)
+    test()
+    return redirect ('/')
