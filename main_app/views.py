a68f46476d4fd1e020f1becc76d4ca47174c5cfb
Nilansh Bansal <bansalnilansh@gmail.com>
2018-04-11 15:48:13 +0530

    prediction complete

--- main_app/views.py
+++ main_app/views.py
@@ -1,20 +1,26 @@
 from django.shortcuts import render,redirect
 from django.conf import settings 
-
+import numpy as np
 import os
 import pickle
 
 def index(request):
     return render(request,'index.html',{'title':'Diabetes Detection'})
 
-def readFile():
-    print ('hello')
-    with open(os.path.join(settings.PROJECT_ROOT, 'bagging_clf.pkl'), 'rb') as f:
-        data = pickle.load(f)
-        print (data)
+def readFile(features):
     
+    with open(os.path.join(settings.PROJECT_ROOT, 'bagging_clf.pkl'), 'rb') as f:
+        model = pickle.load(f)
+        # print (data)
+
+    with open(os.path.join(settings.PROJECT_ROOT, 'scaler.pkl'), 'rb') as f:
+        scaler = pickle.load(f)
+        features = scaler.transform(features)
+        print(model.predict(features))
+        
 
 def predict(request):
-    print (request.POST)
-    readFile()
+    dpf,glucose,bmi,age=float(request.POST.get('dpf')),float(request.POST.get('glucose')),float(request.POST.get('bmi')),float(request.POST.get('age'))
+    features=(np.array([dpf,glucose,bmi,age])).reshape(1, -1)
+    readFile(features)
     return redirect ('/')
